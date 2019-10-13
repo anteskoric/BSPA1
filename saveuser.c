468ba9f1e605429259e20b25c4f4a55cff0ec402
Ante Skoric <44805084+anteskoric@users.noreply.github.com>
2019-10-13 17:16:16 +0200

    Update saveuser.c

--- saveuser.c
+++ saveuser.c
@@ -7,35 +7,27 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 int main () {
-	/*
-	 * echo $UID
-	 * whoami
-	 */
 
 	char input[31];
-	char path[200] = {"C:/Users/agrok/Desktop/Uni Praktika/Semester 3/BSP/Aufgabe01/"};
 	FILE *file;
-	char user[100];
-	char userid[100];
 
-	printf("Enter the file name");
+	printf("Enter the file name: ");
 	fgets(input, 31, stdin);
-	printf(input);
-	/*
-	 * TODO Get user and userid from the linux terminal
-	 */
-
-	file = fopen(strcat(strcat(path,input),".txt"), "w");
+	
+	file = fopen(input, "w");
+	
 	if (file == NULL) {
         perror("Error ocured in opening the file");
-    }
-    else{
-		fprintf(file, strcpy(user,userid));
+    }else{
+        fprintf(file, "%s", getlogin());
+        fprintf(file, " %d", getuid());
         fclose(file);
         printf("Successful input into the file");
     }
-
+	
 	return 0;
 }
